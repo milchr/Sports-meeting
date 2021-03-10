2e5829f520a9bbca6da1df2d199f9436c2c523f0
Czarek98 <cezary.bohdanowicz@gmail.com>
2021-03-10 23:33:12 +0100

    Add Dto and interface to ApplicationUser

---
+++ SportsMeeting/Shared/Dto/ApplicationUser/ApplicationUserDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SportsMeeting.Shared.Dto
+{
+   public class ApplicationUserDto
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
